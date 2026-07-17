eb88810029dc11d79e40ec4fac0c46c10a3f03b8
alexandr-bobkov <sanya8517@yandex.ru>
2026-07-17 16:25:38 +0300

    DEV

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -1,191 +1,172 @@
-# Динамический поиск образа операционной системы Debian 13
-data "yandex_compute_image" "debian_base" {
-  family = var.vm_ubuntu_family # Берет значение "debian-13" из переменных
-}
+# ==============================================================================
+# КОНФИГУРАЦИЯ ВИРТУАЛЬНЫХ МАШИН С КРАСИВЫМИ ИМЕНАМИ ХОСТОВ (HOSTNAME)
+# Студент: Бобков А.К.
+# ==============================================================================
 
-# 4.1 БАСТИОН ХОСТ (Точка входа для SSH Jump)
+# --- 1. КРАСИВЫЙ БАСТИОН-ХОСТ (SECURE JUMP HOST) ---
 resource "yandex_compute_instance" "bastion" {
-  name        = "bastion-host"
-  zone        = var.yc_zone_default
-  platform_id = var.vm_platform_id
+  name        = "bastion-host"     # Имя машины в панели Yandex Cloud
+  hostname    = "bastion-host"     # Внутреннее системное имя ОС Linux
+  platform_id = "standard-v3"
+  zone_id     = "ru-central1-a"
 
-  resources { 
-    cores         = var.vm_cores
-    memory        = var.vm_memory_default
-    core_fraction = var.vm_core_fraction 
+  resources {
+    cores  = 2
+    memory = 2
   }
-
-  boot_disk { 
-    initialize_params { 
-      image_id = data.yandex_compute_image.debian_base.id
-      type     = var.disk_type
-      size     = 15 
-    } 
+  boot_disk {
+    initialize_params {
+      image_id = "fd864g6cmjq6asstq68q" # Чистый Debian 13 Trixie
+      size     = 10
+    }
   }
-
   network_interface {
     subnet_id          = yandex_vpc_subnet.public_a.id
-    nat                = true # Включаем внешний IP
+    nat                = true
     security_group_ids = [yandex_vpc_security_group.bastion_sg.id]
   }
-
-  scheduling_policy { preemptible = true }
-  metadata = { ssh-keys = "debian:${file(var.ssh_public_key_path)}" }
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-# 4.2 ВЕБ-СЕРВЕР 1 (В приватной подсети А)
+# --- 2. ПЕРВЫЙ ОСНОВНОЙ ВЕБ-СЕРВЕР (WEB-1) ---
 resource "yandex_compute_instance" "web_1" {
-  name        = "web-server-1"
-  zone        = var.yc_zone_default
-  platform_id = var.vm_platform_id
+  name        = "web-server-1"     # Имя машины в панели Yandex Cloud
+  hostname    = "web1"             # Внутреннее красивое имя ОС Linux
+  platform_id = "standard-v3"
+  zone_id     = "ru-central1-a"
 
-  resources { 
-    cores         = var.vm_cores
-    memory        = var.vm_memory_default
-    core_fraction = var.vm_core_fraction 
+  resources {
+    cores  = 2
+    memory = 2
   }
-
-  boot_disk { 
-    initialize_params { 
-      image_id = data.yandex_compute_image.debian_base.id
-      type     = var.disk_type
-      size     = 15 
-    } 
+  boot_disk {
+    initialize_params {
+      image_id = "fd864g6cmjq6asstq68q"
+      size     = 15
+    }
   }
-
   network_interface {
     subnet_id          = yandex_vpc_subnet.private_a.id
-    nat                = false # Внешнего IP нет ради безопасности
+    nat                = false # Полностью изолирован в приватной подсети
     security_group_ids = [yandex_vpc_security_group.web_sg.id]
   }
-
-  scheduling_policy { preemptible = true }
-  metadata = { ssh-keys = "debian:${file(var.ssh_public_key_path)}" }
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-# 4.3 ВЕБ-СЕРВЕР 2 (В приватной подсети Б для отказоустойчивости)
+# --- 3. ВТОРОЙ РЕЗЕРВНЫЙ ВЕБ-СЕРВЕР (WEB-2) ---
 resource "yandex_compute_instance" "web_2" {
-  name        = "web-server-2"
-  zone        = var.yc_zone_backup
-  platform_id = var.vm_platform_id
+  name        = "web-server-2"     # Имя машины в панели Yandex Cloud
+  hostname    = "web2"             # Внутреннее красивое имя ОС Linux
+  platform_id = "standard-v3"
+  zone_id     = "ru-central1-b"    # Отказоустойчивость: вынесен в другую зону данных
 
-  resources { 
-    cores         = var.vm_cores
-    memory        = var.vm_memory_default
-    core_fraction = var.vm_core_fraction 
+  resources {
+    cores  = 2
+    memory = 2
   }
-
-  boot_disk { 
-    initialize_params { 
-      image_id = data.yandex_compute_image.debian_base.id
-      type     = var.disk_type
-      size     = 15 
-    } 
+  boot_disk {
+    initialize_params {
+      image_id = "fd864g6cmjq6asstq68q"
+      size     = 15
+    }
   }
-
   network_interface {
     subnet_id          = yandex_vpc_subnet.private_b.id
-    nat                = false # Внешнего IP нет
+    nat                = false
     security_group_ids = [yandex_vpc_security_group.web_sg.id]
   }
-
-  scheduling_policy { preemptible = true }
-  metadata = { ssh-keys = "debian:${file(var.ssh_public_key_path)}" }
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-# 4.4 СЕРВЕР МОНИТОРИНГА (PROMETHEUS)
+# --- 4. СЕРВЕР МОНИТОРИНГА PROMETHEUS ---
 resource "yandex_compute_instance" "prometheus" {
-  name        = "prometheus-server"
-  zone        = var.yc_zone_default
-  platform_id = var.vm_platform_id
+  name        = "prometheus-server" # Имя машины в панели Yandex Cloud
+  hostname    = "prometheus-server" # Внутреннее красивое имя ОС Linux
+  platform_id = "standard-v3"
+  zone_id     = "ru-central1-a"
 
-  resources { 
-    cores         = var.vm_cores
-    memory        = var.vm_memory_default
-    core_fraction = var.vm_core_fraction 
+  resources {
+    cores  = 2
+    memory = 2
   }
-
-  boot_disk { 
-    initialize_params { 
-      image_id = data.yandex_compute_image.debian_base.id
-      type     = var.disk_type
-      size     = 15 
-    } 
+  boot_disk {
+    initialize_params {
+      image_id = "fd864g6cmjq6asstq68q"
+      size     = 20
+    }
   }
-
   network_interface {
     subnet_id          = yandex_vpc_subnet.private_a.id
     nat                = false
     security_group_ids = [yandex_vpc_security_group.internal_mgmt_sg.id]
   }
-
-  scheduling_policy { preemptible = true }
-  metadata = { ssh-keys = "debian:${file(var.ssh_public_key_path)}" }
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-# 4.5 БАЗА ДАННЫХ ЛОГОВ (ELASTICSEARCH)
+# --- 5. БАЗА ДАННЫХ ЛОГОВ ELASTICSEARCH ---
 resource "yandex_compute_instance" "opensearch" {
-  name        = "elasticsearch-logs-server"
-  zone        = var.yc_zone_default
-  platform_id = var.vm_platform_id
+  name        = "elasticsearch-storage" # Имя машины в панели Yandex Cloud
+  hostname    = "elasticsearch-storage" # Внутреннее красивое имя ОС Linux
+  platform_id = "standard-v3"
+  zone_id     = "ru-central1-a"
 
-  resources { 
-    cores         = var.vm_cores
-    memory        = var.vm_memory_large # 4 ГБ ОЗУ под требования базы Elasticsearch
-    core_fraction = var.vm_core_fraction 
+  resources {
+    cores  = 2
+    memory = 4 # Выделено больше ОЗУ под тяжелую Java Virtual Machine базы логов
   }
-
-  boot_disk { 
-    initialize_params { 
-      image_id = data.yandex_compute_image.debian_base.id
-      type     = var.disk_type
-      size     = 20 
-    } 
+  boot_disk {
+    initialize_params {
+      image_id = "fd864g6cmjq6asstq68q"
+      size     = 25
+    }
   }
-
   network_interface {
     subnet_id          = yandex_vpc_subnet.private_a.id
     nat                = false
     security_group_ids = [yandex_vpc_security_group.internal_mgmt_sg.id]
   }
-
-  scheduling_policy { preemptible = true }
-  metadata = { ssh-keys = "debian:${file(var.ssh_public_key_path)}" }
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
-# 4.6 СЕРВЕР С ВЕБ-ПАНЕЛЯМИ (GRAFANA И KIBANA)
+# --- 6. СЕРВЕР ВЕБ-ПАНЕЛЕЙ (GRAFANA / KIBANA) ---
 resource "yandex_compute_instance" "grafana_kibana" {
-  name        = "grafana-kibana-server"
-  zone        = var.yc_zone_default
-  platform_id = var.vm_platform_id
+  name        = "grafana-kibana-server" # Имя машины в панели Yandex Cloud
+  hostname    = "grafana-kibana-server" # Внутреннее красивое имя ОС Linux
+  platform_id = "standard-v3"
+  zone_id     = "ru-central1-a"
 
-  resources { 
-    cores         = var.vm_cores
-    memory        = var.vm_memory_large # 4 ГБ ОЗУ, чтобы держать два тяжелых веб-интерфейса
-    core_fraction = var.vm_core_fraction 
+  resources {
+    cores  = 2
+    memory = 2
   }
-
-  boot_disk { 
-    initialize_params { 
-      image_id = data.yandex_compute_image.debian_base.id
-      type     = var.disk_type
-      size     = 20 
-    } 
+  boot_disk {
+    initialize_params {
+      image_id = "fd864g6cmjq6asstq68q"
+      size     = 20
+    }
   }
-
   network_interface {
     subnet_id          = yandex_vpc_subnet.public_a.id
-    nat                = true # Включаем внешний IP, чтобы админ мог зайти в панели через браузер
+    nat                = true # Нужен внешний IP для открытия веб-интерфейсов из интернета
     security_group_ids = [yandex_vpc_security_group.public_ui_sg.id]
   }
-
-  scheduling_policy { preemptible = true }
-  metadata = { ssh-keys = "debian:${file(var.ssh_public_key_path)}" }
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
 
 # --- АВТОМАТИЧЕСКИЙ ДЕПЛОЙ ИНФРАСТРУКТУРЫ ANSIBLE НА АВТОПИЛОТЕ ---
 resource "null_resource" "ansible_auto_run" {
-  # Ждем, пока все 6 виртуальных машин будут физически созданы в облаке Яндекса
   depends_on = [
     yandex_compute_instance.bastion,
     yandex_compute_instance.web_1,
@@ -200,20 +181,20 @@ resource "null_resource" "ansible_auto_run" {
     command = <<EOT
 cat <<EOF > ../ansible/hosts.ini
 [bastion]
-bastion_host ansible_host=${yandex_compute_instance.bastion.network_interface[0].nat_ip_address} ansible_user=debian
+bastion_host ansible_host=${yandex_compute_instance.bastion.network_interface.nat_ip_address} ansible_user=debian
 
 [webservers]
-web1 ansible_host=${yandex_compute_instance.web_1.network_interface[0].ip_address} ansible_user=debian
-web2 ansible_host=${yandex_compute_instance.web_2.network_interface[0].ip_address} ansible_user=debian
+web1 ansible_host=${yandex_compute_instance.web_1.network_interface.ip_address} ansible_user=debian
+web2 ansible_host=${yandex_compute_instance.web_2.network_interface.ip_address} ansible_user=debian
 
 [logging_storage]
-elasticsearch_server ansible_host=${yandex_compute_instance.opensearch.network_interface[0].ip_address} ansible_user=debian
+elasticsearch_server ansible_host=${yandex_compute_instance.opensearch.network_interface.ip_address} ansible_user=debian
 
 [prometheus_host]
-prometheus_server ansible_host=${yandex_compute_instance.prometheus.network_interface[0].ip_address} ansible_user=debian
+prometheus_server ansible_host=${yandex_compute_instance.prometheus.network_interface.ip_address} ansible_user=debian
 
 [public_mgmt]
-grafana_kibana_server ansible_host=${yandex_compute_instance.grafana_kibana.network_interface[0].ip_address} ansible_user=debian
+grafana_kibana_server ansible_host=${yandex_compute_instance.grafana_kibana.network_interface.ip_address} ansible_user=debian
 EOF
 EOT
   }
